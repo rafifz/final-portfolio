e3356186ca457b075786b1a74afbd2f979ded093
Rafif <56143664+rafifz@users.noreply.github.com>
2024-01-31 09:56:13 +0700

    update insta link

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -10,7 +10,7 @@ export class Footer extends Component {
                     <div className='socialmedia'>
                         <h4>
                         <a href="https://github.com/rafifz"><i className="fab fa-github"> &nbsp;</i></a>
-                        <a href="https://www.instagram.com/ra.fif_/?hl=id"><i className="fab fa-instagram">&nbsp;&nbsp;</i></a>
+                        <a href="https://www.instagram.com/rafif.exe/?hl=id"><i className="fab fa-instagram">&nbsp;&nbsp;</i></a>
                         <a href="https://web.facebook.com/profile.php?id=100007786815332"><i className="fab fa-facebook"> &nbsp;</i></a>
                         <a href="https://www.twitter.com"><i className="fab fa-twitter"> &nbsp;</i></a>
                         <a href="https://www.linkedin.com/"><i className="fab fa-linkedin-in"> &nbsp;</i></a>
